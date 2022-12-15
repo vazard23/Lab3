19cfa7dd2a6d0b253b1f0275b03338785ecc0666
Nazar <bannerfer@gmail.com>
2022-12-15 05:12:04 +0200

    Initial Commit

--- app/src/main/java/com/example/lab3/Drive.kt
+++ app/src/main/java/com/example/lab3/Drive.kt
@@ -20,7 +20,6 @@ class Drive : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentDriveBinding.inflate(layoutInflater, container, false)
-
         return binding.root
     }
 
